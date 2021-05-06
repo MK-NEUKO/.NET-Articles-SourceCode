6e44ad1cd45926719847fd8deb09a1c0e53fd661
Michael Koch <m.koch@know-how-akademie.de>
2021-05-07 00:52:57 +0200

    Projektdateien hinzufügen.

---
+++ AnonymeMethodenLambdas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AnonymeMethodenLambdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Closure/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Closure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
