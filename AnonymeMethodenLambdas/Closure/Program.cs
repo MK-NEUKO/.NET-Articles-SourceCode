93c39023cedee72429518a6af13b237ab447fe58
Michael Koch <m.koch@know-how-akademie.de>
2021-05-12 23:28:03 +0200

    Das Beispiel um meineZahl erweitert.

--- AnonymeMethodenLambdas/Closure/Program.cs
+++ AnonymeMethodenLambdas/Closure/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Closure
 {
-    class Program
+    class Closure
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var planetenImSonnensystem = new List<string>()
+                                 {
+                                     "Merkur","Venus","Erde","Mars",
+                                     "Jupiter","Saturn","Uranus","Neptun",
+                                 };
+
+            Action anonymeMethode = delegate
+            {
+                foreach (var planet in planetenImSonnensystem)
+                {
+                    Console.WriteLine(planet);
+                }
+            };
+
+            anonymeMethode();
+
+
+            int meineZahl = 77;
+
+            Console.WriteLine();
+            Console.WriteLine($"Die Variable \"meineZahl\" vor allen Aufrufen:........{meineZahl}");
+                   
+            Action anonymeMethode2 = () =>
+            {
+                meineZahl++;
+                Console.WriteLine($"In der anonymen Methode nach der Inkrementierung:...{meineZahl}");
+            };
+            anonymeMethode2();
+            Console.WriteLine($"Ausserhalb und nach der anonymen Methode:...........{meineZahl}");
+
+            Console.WriteLine("--------------------------------------------------------");
+            ErhöheMeineZahl(meineZahl);
+            Console.WriteLine($"Ausserhalb und nach ErhöheMeineZahl():............{meineZahl}");           
+        }
+
+        private static void ErhöheMeineZahl(int meineZahl)
+        {
+            meineZahl++;
+            Console.WriteLine($"In ErhöheMeineZahl() nach der Inkrementierung:....{meineZahl}");
         }
     }
 }
