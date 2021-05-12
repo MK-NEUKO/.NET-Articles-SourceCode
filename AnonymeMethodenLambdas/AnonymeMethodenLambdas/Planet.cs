ae6d1ae9c6d2522a5931738d3d07fb4c275354de
Michael Koch <m.koch@know-how-akademie.de>
2021-05-12 23:30:52 +0200

    KLassen in eigene Datein ausgelagert.

---
+++ AnonymeMethodenLambdas/AnonymeMethodenLambdas/Planet.cs
@@ -0,0 +1,25 @@
+﻿namespace AnonymeMethodenLambdas
+{
+    public class Planet
+    {
+        private readonly string _name;
+        private readonly string _kategorie;
+        private readonly int _durchmesser;
+
+        public Planet(string name, string kategorie, int durchmesser)
+        {
+            _name = name;
+            _kategorie = kategorie;
+            _durchmesser = durchmesser;
+        }
+
+        public string Name => _name;
+        public string Kategorie => _kategorie;
+        public int Durchmesser => _durchmesser;
+
+        public override string ToString()
+        {
+            return $" {_name.PadRight(8)}| {_kategorie.PadRight(15)}| Durchmesser:{_durchmesser.ToString().PadLeft(7, '.')} km";
+        }
+    }
+}
--- AnonymeMethodenLambdas/AnonymeMethodenLambdas/Program.cs
+++ AnonymeMethodenLambdas/AnonymeMethodenLambdas/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
 
 namespace AnonymeMethodenLambdas
@@ -62,113 +61,6 @@ namespace AnonymeMethodenLambdas
             planetenVerwalter.ErstelleDurchmessersortiertePlanetenkategorien(planetenImSonnensystem);
 
             planetenVerwalter.ErstelleDurchmessersortiertePlanetenkategorienLambda(planetenImSonnensystem);
-        }
-    }
-
-    public class Planet
-    {
-        private readonly string _name;
-        private readonly string _kategorie;
-        private readonly int _durchmesser;
-
-        public Planet(string name, string kategorie, int durchmesser)
-        {
-            _name = name;
-            _kategorie = kategorie;
-            _durchmesser = durchmesser;
-        }
-
-        public string Name => _name;
-        public string Kategorie => _kategorie;
-        public int Durchmesser => _durchmesser;
-
-        public override string ToString()
-        {
-            return $" {_name.PadRight(8)}| {_kategorie.PadRight(15)}| Durchmesser:{_durchmesser.ToString().PadLeft(7, '.')} km";
-        }
-    }
-
-    public class PlanetenVerwalter
-    {
-        public void ErstelleDurchmessersortiertePlanetenkategorienLambda(IReadOnlyList<Planet> planetenListe)
-        {
-            var kategorisiertePlaneten = planetenListe.Where(planet => planet.Kategorie != "Zwergplanet")
-                                                      .OrderByDescending(planet => planet.Durchmesser)
-                                                      .GroupBy(planet => planet.Kategorie);
-
-            GibKategorisiertePlanetenAus(kategorisiertePlaneten);
-        }
-
-        public void ErstelleDurchmessersortiertePlanetenkategorien(IReadOnlyList<Planet> planetenListe)
-        {
-            // Erzeugen eines Dictionary zur aufnahme der Kategorie als Key und der Liste als Value.
-            var kategorisiertePlaneten = new Dictionary<string, List<Planet>>();
-
-            foreach (var planet in planetenListe)
-            {
-                // Zwergplaneten ausschließen
-                if (planet.Kategorie == "Zwergplanet")
-                {
-                    continue;
-                }
-
-                // Ist die endsprechende Kategorie im Dictionary nicht vorhanden,
-                // dann eine List von Planeten unter dieser Kategorie initialisieren.
-                if (kategorisiertePlaneten.ContainsKey(planet.Kategorie) == false)
-                {
-                    kategorisiertePlaneten[planet.Kategorie] = new List<Planet>();
-                }
-
-                // Den Planeten zu der Liste hinzufügen
-                kategorisiertePlaneten[planet.Kategorie].Add(planet);
-            }
-
-            // Fertige Liste einer Kategorie nach Durchmesser sortieren.
-            foreach (var liste in kategorisiertePlaneten.Values)
-            {
-                liste.Sort(SortiereNachDurchmesser);
-            }
-
-            GibKategorisiertePlanetenAus(kategorisiertePlaneten);
-        }
-
-        private static void GibKategorisiertePlanetenAus(Dictionary<string, List<Planet>> kategorisiertePlaneten)
-        {
-            foreach (var kategorieUndListe in kategorisiertePlaneten)
-            {
-                var planetenKategorie = kategorieUndListe.Key;
-                Console.WriteLine($"Kategorie: {planetenKategorie}");
-                Console.WriteLine("-----------------------------------");
-
-                foreach (var liste in kategorieUndListe.Value)
-                {
-                    Console.WriteLine($" {liste.Name.PadRight(7)} | Durchmesser:{liste.Durchmesser.ToString().PadLeft(8, '.')} km");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        private static void GibKategorisiertePlanetenAus(IEnumerable<IGrouping<string, Planet>> kategorisiertePlaneten)
-        {
-            Console.WriteLine("ErstelleDurchmessersortiertePlanetenkategorienLambda(IReadOnlyList<Planet> planetenListe)");
-            Console.WriteLine();
-            foreach (var kategorieUndListe in kategorisiertePlaneten)
-            {
-                var planetenKategorie = kategorieUndListe.Key;
-                Console.WriteLine($"Kategorie: {planetenKategorie}");
-                Console.WriteLine("-----------------------------------");
-
-                foreach (var liste in kategorieUndListe)
-                {
-                    Console.WriteLine($" {liste.Name.PadRight(7)} | Durchmesser:{liste.Durchmesser.ToString().PadLeft(8, '.')} km");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        private int SortiereNachDurchmesser(Planet ersterPlanet, Planet zweiterPlanet)
-        {
-            return zweiterPlanet.Durchmesser - ersterPlanet.Durchmesser;
         }
     }
 }
